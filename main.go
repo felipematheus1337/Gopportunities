0e857e26d58de356db3eec18f334f0f9d80911dc
felipematheus1337 <lipehbr@gmail.com>
2025-12-03 13:07:37 -0300

    Criado logger personalizado, schema da entity

--- main.go
+++ main.go
@@ -1,8 +1,24 @@
 package main
 
-import "github.com/felipematheus1337/Gopportunities/router"
+import (
+	"github.com/felipematheus1337/Gopportunities/config"
+	"github.com/felipematheus1337/Gopportunities/router"
+)
+
+var (
+	logger config.Logger
+)
 
 func main() {
+
+	logger = *config.GetLogger("main")
+
+	err := config.Init()
+
+	if err != nil {
+		logger.Errorf("config initialization error", err)
+		return
+	}
 
 	router.Initialize()
 }
