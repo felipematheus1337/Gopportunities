63162d29d9d5d79c6ed1bb6bc3afb7e2c9091179
felipematheus1337 <lipehbr@gmail.com>
2025-12-03 13:22:22 -0300

    Criado configuração do SQLITE e GITIGNORE.

--- config/config.go
+++ config/config.go
@@ -1,6 +1,8 @@
 package config
 
 import (
+	"fmt"
+
 	"gorm.io/gorm"
 )
 
@@ -10,10 +12,22 @@ var (
 )
 
 func Init() error {
+	var err error
+
+	db, err = InitializeSQLite()
+
+	if err != nil {
+		return fmt.Errorf("error initializing sqlite database: %v", err)
+	}
+
 	return nil
 }
 
 func GetLogger(p string) *Logger {
 	logger = NewLogger(p)
 	return logger
+}
+
+func GetSQLite() *gorm.DB {
+	return db
 }
