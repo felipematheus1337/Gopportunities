2e652ad8f3ec03cb744a7b67a8802433076fb3bd
felipematheus1337 <lipehbr@gmail.com>
2025-12-03 10:49:01 -0300

    criado routes, com as rotas da aplicação.

--- router/router.go
+++ router/router.go
@@ -5,11 +5,7 @@ import "github.com/gin-gonic/gin"
 func Initialize() {
 	router := gin.Default()
 
-	router.GET("/ping", func(ctx *gin.Context) {
-		ctx.JSON(200, gin.H{
-			"message": "pong",
-		})
-	})
+	initializeRoutes(router)
 
 	router.Run(":8080")
 }
---
+++ router/routes.go
@@ -0,0 +1,34 @@
+package router
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func initializeRoutes(router *gin.Engine) {
+
+	v1 := router.Group("/api/v1")
+	{
+
+		v1.GET("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{"message": "opening"})
+		})
+		v1.POST("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{"message": "opening"})
+		})
+		v1.DELETE("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{"message": "opening"})
+		})
+
+		v1.PUT("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{"message": "opening"})
+		})
+
+		v1.GET("/opening", func(ctx *gin.Context) {
+			ctx.JSON(http.StatusOK, gin.H{"message": "opening"})
+		})
+
+	}
+
+}
